46ee8a4cb08413edc8b0800627ffe05a2b7be886
Andersmb <anders.brakestad@gmail.com>
2019-12-03 00:26:12 +0100

    added content as attribute instead of method

--- queuegui/output_parsers/orca.py
+++ queuegui/output_parsers/orca.py
@@ -4,18 +4,16 @@ class OrcaOut(object):
     def __init__(self, filename):
         self.filename = filename
 
+        with open(self.filename) as f:
+            self.source = f.read()
+            self.content = self.source.splitlines()
+
     def __repr__(self):
         return "<OrcaOut(filename={})>".format(self.filename)
 
-    def content(self):
-        """Return a generator that iterates over lines in the file, one by one."""
-        with open(self.filename, "r") as f:
-            while True:
-                yield next(f)
-    
     def geometry_trajectory(self):
         """Return list of all geometry steps from a geometry optimization. The last step is the optimized geometry"""
-        output = list(self.content())
+        output = self.content
         traj = []
         for i, line in enumerate(output):
             if line.strip().startswith("CARTESIAN COORDINATES (ANGSTROEM)"):
@@ -32,76 +30,76 @@ class OrcaOut(object):
         """Evaluate whether the job
         terminated normally, and return a Boolean:
         True if termination was normal, False if not."""
-        if list(self.content())[-1].strip().startswith("TOTAL RUN TIME:"):
+        if self.content[-1].strip().startswith("TOTAL RUN TIME:"):
             return True
         return False
     
     def scf_energy(self):
         """Return a list of floats containing the optimized SCF energies.
         Note that these values INCLUDE the dispersion correction if present."""
-        e = filter(lambda x: x.strip().startswith("FINAL SINGLE POINT ENERGY"), self.content())
+        e = filter(lambda x: x.strip().startswith("FINAL SINGLE POINT ENERGY"), self.content)
         return list(map(float, map(lambda x: x.split()[4], e)))
 
     def no_scfcycles(self):
         """Return a list of floats containing the number of SCF iterations needed for converging each geom step"""
-        c = filter(lambda x: ' '.join(x.split()).startswith("* SCF CONVERGED AFTER"), self.content())
+        c = filter(lambda x: ' '.join(x.split()).startswith("* SCF CONVERGED AFTER"), self.content)
         return list(map(int, map(lambda x: x.split()[4], c)))
 
     def walltime(self):
         """Return the total walltime for the job (float) in seconds"""
-        w = [line for line in self.content() if line.strip().startswith("TOTAL RUN TIME:")][0].split()
+        w = [line for line in self.content if line.strip().startswith("TOTAL RUN TIME:")][0].split()
         return float(w[3])*86400 + float(w[5])*3600 + float(w[7])*60 + float(w[9]) + float(w[11])/1000
 
     def no_atoms(self):
         """Return the number of atoms of the system (integer)"""
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("Number of atoms"):
                 return int(line.strip().split()[-1])
              
     def no_geomcycles(self):
         """Return the number of geometry cycles needed for convergence. Return an integer."""
-        return len(self.geometry_trjectory())
+        return len(self.geometry_trajectory())
     
     def no_basisfunctions(self):
         """Return the number of basis functions (integer)."""
         nbasis = None
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("Basis Dimension"):
                 return int(line.strip().split()[-1])
 
     def maxforce(self):
         """Return a list of floats containing all Max Forces for each geometry iteration"""
-        l = filter(lambda x: x.strip().startswith("MAX gradient"), self.content())
+        l = filter(lambda x: x.strip().startswith("MAX gradient"), self.content)
         l = filter(lambda x: len(x.split()) > 4, l)
         return list(map(float, map(lambda x: x.strip().split()[2], l)))
 
     def rmsforce(self):
         """Return a list of floats containing all RMS Forces for each geometry iteration"""
-        l = filter(lambda x: x.strip().startswith("RMS gradient"), self.content())
+        l = filter(lambda x: x.strip().startswith("RMS gradient"), self.content)
         l = filter(lambda x: len(x.split()) > 4, l)
         return list(map(float, map(lambda x: x.strip().split()[2], l)))
     
     def maxstep(self):
         """Return a list of floats containing all Max Steps for each geometry iteration"""
-        l = filter(lambda x: x.strip().startswith("MAX step"), self.content())
+        l = filter(lambda x: x.strip().startswith("MAX step"), self.content)
         return list(map(float, map(lambda x: x.strip().split()[2], l)))
     
     def rmsstep(self):
         """Return a list of floats containing all RMS Steps for each geometry iteration"""
-        l = filter(lambda x: x.strip().startswith("RMS step"), self.content())
+        l = filter(lambda x: x.strip().startswith("RMS step"), self.content)
         return list(map(float, map(lambda x: x.strip().split()[2], l)))
 
     def tol_maxforce(self):
         """Return the Max Force convergence tolerance as float"""
         tol = None
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("MAX gradient") and len(line.split()) > 4:
                 return float(line.strip().split()[3])
 
     def tol_rmsforce(self):
         """Return the RMSD Force convergence tolerance as float"""
         tol = None
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("RMS gradient") and len(line.split()) > 4:
                 return float(line.strip().split()[3])
 
@@ -109,7 +107,7 @@ class OrcaOut(object):
         """Return the Max Step convergence tolerance as float
         :return: float
         """
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("MAX step"):
                 return float(line.strip().split()[3])
 
@@ -117,7 +115,7 @@ class OrcaOut(object):
         """Return the RMSD Step convergence tolerance as float
         :return: float
         """
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("RMS step"):
                 return float(line.strip().split()[3])
 
@@ -126,7 +124,7 @@ class OrcaOut(object):
         Return the version of ORCA as printed at the top of the output file
         :return: str
         """
-        for line in self.content():
+        for line in self.content:
             if line.strip().startswith("Program Version"):
                 return line.strip()
 
@@ -135,7 +133,7 @@ class OrcaOut(object):
         Return the Energy change SCF convergence tolerance
         :return: float
         """
-        for line in self.content():
+        for line in self.content:
             if line.split()[:4] == ["Energy", "Change", "TolE", "...."]:
                 return float(line.split()[4])
 
@@ -144,7 +142,7 @@ class OrcaOut(object):
         Return the 1-Electron energy change SCF convergence tolerance
         :return: float
         """
-        for line in self.content():
+        for line in self.content:
             if line.split()[:4] == ["1-El.", "energy", "change", "...."]:
                 return float(line.split()[4])
 
@@ -153,12 +151,12 @@ class OrcaOut(object):
         Return each set of SCF optimization cycle summaries.
         :return: list
         """
-        output = list(self.content())
+        output = self.content
         indeces = []
-        for i, line in enumerate(list(self.content())):
+        for i, line in enumerate(self.content):
             if line.strip().startswith("SCF ITERATIONS"):
                 start = i + 3
-                for j in xrange(200):
+                for j in range(200):
                     try:
                         if "***" in output[start+j] and ("Energy Check signals convergence" not in output[start+j] or "convergence achieved" not in output[start+j]):
                             continue
@@ -179,7 +177,7 @@ class OrcaOut(object):
         return [[line.strip() for line in cycle if not "***" in line] for cycle in data]
 
     def polarizability_diagonal(self):
-        output = list(self.content())
+        output = self.content
         diag = []
         for i, line in enumerate(output):
             if line.strip().startswith("The raw cartesian tensor"):
@@ -194,7 +192,7 @@ class OrcaOut(object):
         raise NoDispersionCorrection exception.
         :return:
         """
-        for line in reversed(list(self.content())):
+        for line in reversed(self.content):
             if line.strip().startswith("Dispersion correction  ") and len(line.split()) == 3:
                 return float(line.split()[-1])
         else:
@@ -206,7 +204,7 @@ class OrcaOut(object):
         raise NoZPECorrection exception.
         :return:
         """
-        for line in reversed(list(self.content())):
+        for line in reversed(self.content):
             if line.strip().startswith("Non-thermal (ZPE) correction"):
                 return float(line.split()[3])
         else:
@@ -217,7 +215,7 @@ class OrcaOut(object):
         Return the final total energy. Note that this value does NOT INCLUDE the D3 correction.
         :return:
         """
-        for line in reversed(list(self.content())):
+        for line in reversed(self.content):
             if line.strip().startswith("Total Energy"):
                 return float(line.split()[3])
         else:
@@ -230,7 +228,7 @@ class OrcaOut(object):
         :param var: name or ORCA variable used for storing BSSE
         :return:
         """
-        content = list(reversed(list(self.content())))
+        content = list(reversed(self.content))
         for i, line in enumerate(content):
             if line.strip().startswith("Variable Name") and line.split()[-1] == var.upper():
                 if content[i-13].strip().startswith("Value"):
@@ -244,7 +242,7 @@ class OrcaOut(object):
         :return:
         """
         t = []
-        content = list(self.content())
+        content = self.content
         for i, line in enumerate(content):
             if line.strip().startswith("Timings for individual modules:"):
                 t.append(float(content[i+4].split()[3]))
@@ -256,7 +254,7 @@ class OrcaOut(object):
         :return:
         """
         c = []
-        for line in self.content():
+        for line in self.content:
             if "%Pal NProcs" in line:
                 c.append(int(line.split()[4]))
         return c
@@ -266,30 +264,27 @@ class OrcaHess(object):
     def __init__(self, filename):
         self.filename = filename
 
-    def content(self):
-        """Return a generator that iterates over lines in the file, one by one."""
-        with open(self.filename, "r") as f:
-            while True:
-                yield next(f)
+        with open(self.filename) as f:
+            self.source = f.read()
+            self.content = self.source.splitlines()
 
     def no_freq(self):
-        content = list(self.content())
+        content = self.content
         for i, line in enumerate(content):
             if line.strip().startswith("$vibrational_frequencies"):
                 return int(content[i+1].strip())
 
     def normal_modes(self):
-        content = list(self.content())
         no_freq = self.no_freq()
 
         freqs, rawmodes = [], []
         modes = [[] for _i in range(no_freq)]
 
-        for i, line in enumerate(content):
+        for i, line in enumerate(self.content):
             # Get the frequencies
             if line.startswith("$vibrational_frequencies"):
                 for j in range(no_freq):
-                    freqs.append(float(content[i+j+2].split()[1]))
+                    freqs.append(float(self.content[i+j+2].split()[1]))
 
             # Now get all the normal modes
             if line.startswith("$normal_modes"):
@@ -297,7 +292,7 @@ class OrcaHess(object):
                 start = i + 3
 
                 # The number of columns containing normal modes
-                cols = len(content[start].split()) - 1
+                cols = len(self.content[start].split()) - 1
 
                 # The number of times the columns are repeated (rows)
                 rows = int(no_freq / cols) + 1
@@ -313,13 +308,13 @@ class OrcaHess(object):
                     start = i + 3 + r * (no_freq + 1)
                     for c in range(cols):
                         for freq in range(no_freq):
-                            rawmodes.append(float(content[start+freq].split()[c+1]))
+                            rawmodes.append(float(self.content[start+freq].split()[c+1]))
 
                 # Now pick up the  "rest" normal modes
                 start = i + 3 + (rows - 1) * (no_freq + 1)
                 for r in range(rest):
                     for freq in range(no_freq):
-                        rawmodes.append(float(content[start + freq].split()[r + 1]))
+                        rawmodes.append(float(self.content[start + freq].split()[r + 1]))
 
                 # Now split the normal mode data into their x, y, and z components, as
                 # this will make it easier to write to file
@@ -336,11 +331,11 @@ class OrcaHess(object):
         return modes
 
     def geometry(self):
-        content = list(self.content())
+        content = self.content
         geom = []
         for i, line in enumerate(content):
             if line.strip().startswith("$atoms"):
-                no_atoms = int(content[i+1])
+                no_atoms = int(self.content[i+1])
                 for atom in range(no_atoms):
                     geom.append(content[i+2+atom])
 
@@ -348,7 +343,7 @@ class OrcaHess(object):
         return [atom[0] + " " + " ".join(atom[2:]) for atom in geom]
 
     def frequencies(self):
-        content = list(self.content())
+        content = self.content
         freq = []
         for i, line in enumerate(content):
             if line.strip().startswith("$vibrational_frequencies"):
