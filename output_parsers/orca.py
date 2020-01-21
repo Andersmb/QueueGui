ab8d8296ce91945ce38e369ba7576fa2387a6866
Andersmb <anders.brakestad@gmail.com>
2020-01-21 10:05:52 +0100

    now find helpers

--- output_parsers/orca.py
+++ output_parsers/orca.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "queuegui")
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), , "queuegui"))
 
 import helpers
 
